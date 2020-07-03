7e4d841986aa035edc6701d0f84cc457f1087baa
russellrockwood <you@example.com>
2020-07-03 15:47:20 -0700

    updated models

--- volunteer_philippines/volunteerapp/models.py
+++ volunteer_philippines/volunteerapp/models.py
@@ -1,18 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
-
-
-# class User(models.Model):
-#     first_name = models.CharField(max_length=50)
-#     last_name = models.CharField(max_length=50) 
-#     email = models.CharField(max_length=50)
-#     birthday = models.DateField()  
-#     password = models.CharField(max_length=10)    
-
-#     def __str__(self):
-#         return self.first_name + ' ' + self.last_name
-
+from django.conf import settings
 
 
 class Post(models.Model):
@@ -20,10 +8,10 @@ class Post(models.Model):
     picture = models.ImageField(upload_to='images/', max_length=150,)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    # author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default='')
 
     def __str__(self):
-        return self.title 
+        return self.title + self.author
 
 
 
