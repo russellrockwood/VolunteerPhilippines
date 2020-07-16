474ed5d1d627b5d567fae14824d79411f861008e
Russell Rockwood <russell.rockwood@gmail.com>
2020-07-16 10:53:20 -0700

    added news stories

--- volunteer_philippines/volunteer_philippines/urls.py
+++ volunteer_philippines/volunteer_philippines/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('volunteerapp/', include('volunteerapp.urls'))
+    path('', include('volunteerapp.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- volunteer_philippines/volunteerapp/urls.py
+++ volunteer_philippines/volunteerapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'volunteerapp'
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('news/', views.news, name='news'),
     path('login/', views.login_page, name='login_page'),
     path('register/', views.register, name='register'),
